d6fc37ea7c0c5cfabe67beb4caf624e920de0197
Kaickpl <kaick.mlopes@upe.br>
2025-09-08 15:37:24 -0300

    Iniciando projeto

---
+++ src/test/java/br/com/upe/academia/AcademiaWeb/AcademiaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.upe.academia.AcademiaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
