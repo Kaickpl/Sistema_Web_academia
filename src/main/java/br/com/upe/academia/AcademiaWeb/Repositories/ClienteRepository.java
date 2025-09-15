7a7f985c7e2ae3c45d1d44cdffdb4fe30d832dbb
Kaickpl <kaick.mlopes@upe.br>
2025-09-15 14:36:07 -0300

    Ajuste de classe

--- src/main/java/br/com/upe/academia/AcademiaWeb/Repositories/ClienteRepository.java
+++ src/main/java/br/com/upe/academia/AcademiaWeb/Repositories/ClienteRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository <Cliente, UUID> {
+public interface ClienteRepository extends JpaRepository <Cliente, UUID> {
 
 
 }
