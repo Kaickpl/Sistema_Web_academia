ef948cd482d4174cb33cabc0cbc97513aef87bd6
Glauco Siqueira <glauco0909@gmail.com>
2025-10-08 21:03:23 -0300

    corrigindo Serie

--- src/main/java/br/com/upe/academia/AcademiaWeb/Entities/LogicaTreinos/Serie.java
+++ src/main/java/br/com/upe/academia/AcademiaWeb/Entities/LogicaTreinos/Serie.java
@@ -77,33 +77,5 @@ public class Serie {
     public void setExercicio(Exercicio exercicio) {
         this.exercicio = exercicio;
     }
-
-    public UUID getIdSerie() {
-        return idSerie;
-    }
-
-    public void setIdSerie(UUID idSerie) {
-        this.idSerie = idSerie;
-    }
-
-    public void setConcluida(boolean concluida) {
-        isConcluida = concluida;
-    }
-
-    public Integer getNumeroDeRepeticoes() {
-        return numeroDeRepeticoes;
-    }
-
-    public void setNumeroDeRepeticoes(Integer numeroDeRepeticoes) {
-        this.numeroDeRepeticoes = numeroDeRepeticoes;
-    }
-
-    public Float getPesoDaSerie() {
-        return pesoDaSerie;
-    }
-
-    public void setPesoDaSerie(Float pesoDaSerie) {
-        this.pesoDaSerie = pesoDaSerie;
-    }
 }
 
