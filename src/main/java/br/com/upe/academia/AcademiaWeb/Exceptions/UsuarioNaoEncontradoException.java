0d47c939f76e899beacf7289e2645994d64e2bf3
Kaickpl <kaick.mlopes@upe.br>
2025-11-01 09:34:49 -0300

    adicionado classes de exceções genéricas para regras de negócio e validação.

---
+++ src/main/java/br/com/upe/academia/AcademiaWeb/Exceptions/UsuarioNaoEncontradoException.java
@@ -0,0 +1,10 @@
+package br.com.upe.academia.AcademiaWeb.Exceptions;
+
+public class UsuarioNaoEncontradoException extends RuntimeException {
+    public UsuarioNaoEncontradoException(String message) {
+        super(message);
+    }
+    public UsuarioNaoEncontradoException() {
+        super("Usuario não encontrado");
+    }
+}
