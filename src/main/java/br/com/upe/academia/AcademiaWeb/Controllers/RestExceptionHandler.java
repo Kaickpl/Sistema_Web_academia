7b9f073f3b465e4027a045b633bf69e7380f46c5
Gabi <gabimelo640@gmail.com>
2025-11-01 20:17:24 -0300

    fiz mais babados de excessão

--- src/main/java/br/com/upe/academia/AcademiaWeb/Controllers/RestExceptionHandler.java
+++ src/main/java/br/com/upe/academia/AcademiaWeb/Controllers/RestExceptionHandler.java
@@ -6,10 +6,12 @@ import jakarta.persistence.ElementCollection;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -51,6 +53,22 @@ public class RestExceptionHandler {
     @ExceptionHandler(MedidaInvalidaException.class)
     public ResponseEntity<ExceptionResponseDTO> handleMedidaInvalidaException(MedidaInvalidaException ex, HttpServletRequest request) {
         ExceptionResponseDTO exceptionResponseDTO = new ExceptionResponseDTO(ex.getMessage(), 400, request.getRequestURI());
+        return ResponseEntity.status(exceptionResponseDTO.getStatus()).body(exceptionResponseDTO);
+    }
+
+    @ExceptionHandler (NullPointerException.class)
+    public ResponseEntity<ExceptionResponseDTO> handleCampoVazio(NullPointerException ex, HttpServletRequest request) {
+        ExceptionResponseDTO exceptionResponseDTO = new ExceptionResponseDTO("Erro de campo vazio: Verifique se todos os campos obrigatórios foram devidamente preenchidos.",
+                500, request.getRequestURI());
+        return ResponseEntity.status(exceptionResponseDTO.getStatus()).body(exceptionResponseDTO);
+    }
+
+    @ExceptionHandler (HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponseDTO> handleTipoErrado(HttpMessageNotReadableException ex, HttpServletRequest request) {
+        ExceptionResponseDTO exceptionResponseDTO = new ExceptionResponseDTO(
+                "Erro de formato: Verifique se todos os campos numéricos foram enviados como número.",
+                400, request.getRequestURI()
+        );
         return ResponseEntity.status(exceptionResponseDTO.getStatus()).body(exceptionResponseDTO);
     }
 }
